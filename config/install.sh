ef9d21da0eb7b4c817e769d2d91ec155b93438a9
im2058 <10438075@qq.com>
2018-12-08 14:59:00 +0000

    ./*

--- config/install.sh
+++ config/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-###for xigua
+#for youtube
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
+sudo apt-get install -y shadowsocks privoxy 
+###for xigua
 sudo apt-get install -y vim tmux samba phantomjs python3-selenium
 sudo apt-get install -y python3-dev build-essential libssl-dev libffi-dev libxml2 libxml2-dev libxslt1-dev zlib1g-dev libcurl4-openssl-dev
 sudo apt-get install -y python3 python3-pip aria2 ntfs-3g
@@ -9,5 +12,3 @@ sudo pip3 install pyquery
 sudo rm -f /usr/bin/python
 sudo ln -s /usr/bin/python3.5 /usr/bin/python
 
-#for youtube
-#sudo apt-get install -y shadowsocks privoxy 
