1cf15dba18e9d7ebab7e70585f4f2612c4c9f330
im2058 <10438075@qq.com>
2018-12-09 02:11:09 +0000

    ./*

--- config/install.sh
+++ config/install.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y shadowsocks privoxy
 sudo apt-get install -y vim tmux samba phantomjs python3-selenium
 sudo apt-get install -y python3-dev build-essential libssl-dev libffi-dev libxml2 libxml2-dev libxslt1-dev zlib1g-dev libcurl4-openssl-dev
 sudo apt-get install -y python3 python3-pip aria2 ntfs-3g
-sudo pip3 install pyquery
+sudo pip3 install pyquery you-get
 sudo rm -f /usr/bin/python
 sudo ln -s /usr/bin/python3.5 /usr/bin/python
 
